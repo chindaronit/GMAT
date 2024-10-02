329fef55f3dd75b27afedde7b5bfb76cfd7bf889
chinda <ronitchinda100@gmail.com>
2024-10-03 03:25:23 +0530

    Updated user.controller.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import {
-  getUser,
+  getUserByVPA,
+  getUserByPhone,
   addUser,
   updateUser,
 } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
-userRouter.route("/").get(getUser).post(addUser).put(updateUser);
+userRouter.route("/").post(addUser).put(updateUser);
+userRouter.route("/get/vpa").get(getUserByVPA);
+userRouter.route("/get/ph").get(getUserByPhone);
 
-export default userRouter
+export default userRouter;

6c3b63fcb2f2142a5512d9efe73ad1c67681f1fe
vishal2537 <vishal.prajapati21b@iiitg.ac.in>
2024-10-06 14:42:26 +0530

    updated rewards

--- boto3/ApiGateway/Rewards.py
+++ boto3/ApiGateway/Rewards.py
@@ -26,12 +26,25 @@ def leaderboard_api(client, apiId, rootResourceId, authorizationType, contentTyp
     status_codes = ['500', '400', '401']
     for statusCode in status_codes:
         other_response(client, apiId, leaderboardResourceId, httpMethod, statusCode, contentType, Model)
+        
+    requestParameters = {
+        'method.request.querystring.userId': True,
+        'method.request.querystring.month': True,
+        'method.request.querystring.year': True,
+    }
 
     # /leaderboard GET
     httpMethod = 'GET'
     integrationHttpMethod = 'GET'
     leaderboard_get_url = url + 'leaderboard'
     putMethod(client, apiId, authorizationType, leaderboardResourceId, httpMethod, requestParameters, requestModels)
+    
+    requestParameters = {
+        'integration.request.querystring.userId':'method.request.querystring.userId',
+        'integration.request.querystring.month':'method.request.querystring.month',
+        'integration.request.querystring.year':'method.request.querystring.year'
+    }
+    
     putIntegration(client, apiId, httpMethod, leaderboardResourceId, type, integrationHttpMethod, leaderboard_get_url, passthroughBehavior, requestParameters)
     succ_response(client, apiId, leaderboardResourceId, httpMethod, contentType, Model)
 
@@ -44,11 +57,21 @@ def leaderboard_api(client, apiId, rootResourceId, authorizationType, contentTyp
     #                     /leaderboard/all GET
     # ***************************************************************
 
+    requestParameters = {
+        'method.request.querystring.month': True,
+        'method.request.querystring.year': True,
+    }
+
     allResourceId = create_resource(client, apiId, leaderboardResourceId, "all")
     httpMethod = 'GET'
     integrationHttpMethod = 'GET'
     leaderboard_all_url = url + 'leaderboard/all'
     putMethod(client, apiId, authorizationType, allResourceId, httpMethod, requestParameters, requestModels)
+    requestParameters = {
+        'integration.request.querystring.month':'method.request.querystring.month',
+        'integration.request.querystring.year':'method.request.querystring.year'
+    }
+    
     putIntegration(client, apiId, httpMethod, allResourceId, type, integrationHttpMethod, leaderboard_all_url, passthroughBehavior, requestParameters)
     succ_response(client, apiId, allResourceId, httpMethod, contentType, Model)
 

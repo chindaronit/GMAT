cf3afa798c5771ff3163e9b986b132c0d3ae8947
vishal2537 <vishal.prajapati21b@iiitg.ac.in>
2024-10-10 22:14:01 +0530

    added LeaderboardEvents TransactionEvents

--- app/src/main/java/com/gmat/ui/events/LeaderboardEvents.kt
+++ app/src/main/java/com/gmat/ui/events/LeaderboardEvents.kt
@@ -5,4 +5,7 @@ import com.gmat.data.model.LeaderboardModel
 sealed class LeaderboardEvents {
     data class GetLeaderboardThroughUserId(val userId: String) : LeaderboardEvents()
     data class UpdateLeaderboard(val leaderboard: LeaderboardModel) : LeaderboardEvents()
+    data class GetUserRewardsPointsForMonth(val userId: String, val month: Int, val year: Int) : LeaderboardEvents()
+    data class GetAllUsersByRewardsForMonth(val month: Int, val year: Int) : LeaderboardEvents()
+    data class AddUserTransactionRewards(val userId: String, val rewards: Double) : LeaderboardEvents()
 }
--- app/src/main/java/com/gmat/ui/events/TransactionEvents.kt
+++ app/src/main/java/com/gmat/ui/events/TransactionEvents.kt
@@ -2,9 +2,11 @@ package com.gmat.ui.events
 
 import com.gmat.data.model.TransactionModel
 
-
 sealed class TransactionEvents {
     data class AddTransaction(val transaction: TransactionModel) : TransactionEvents()
-
+    data class GetTransactionById(val txnId: String) : TransactionEvents()
+    data class GetAllTransactionsForUser(val userId: String) : TransactionEvents()
+    data class GetAllTransactionsForMonth(val month: Int, val year: Int) : TransactionEvents()
+    data class GetUserTransactionsByUserIdAndPayeeId(val userId: String, val payeeId: String) : TransactionEvents()
+    data class GetAllTransactionsForPayerIdAndPayeeId(val payerId: String, val payeeId: String) : TransactionEvents()
 }
-
